179950a978447eeab237c6493b6b7a16288803cd
Django Koenig <django.koenig@gmail.com>
2019-08-13 20:18:18 -0400

    sticky nav --> top nav

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -5,13 +5,12 @@ import './Toolbar.css';
 const toolbar = props => (
   <header className="toobar">
      <nav className="toolbar_navigation">
-        <div></div>
         <div className="toolbar_logo"><a href="/">HOPE WORKS</a></div>
         <div className="toolbar_navigation-items">
-          <ul>
-            <li><a href="#all-fields">Personal Information</a></li>
+          <ul className="toolbar_list-items">
+            <li><a href="#all-fields">Personal Info</a></li>
             <li><a href="#demographic-content">Demographic</a></li> 
-            <li><a href="#incident-information">Incident Information</a></li> 
+            <li><a href="#incident-information">Incident Info</a></li> 
             <li><a href="#ongoing-services">Communication Details</a></li> 
             <li><a href="#services-provided">Services Provided</a></li> 
             <li><a href="#referrals-h">Referrals</a></li> 
