2f348c691c0cbe723ffd7540254ce7b5b1a972c2
cwissell <51326406+cwissell@users.noreply.github.com>
2019-08-19 16:17:55 -0400

    data page getting close

--- src/data.js
+++ src/data.js
@@ -4,9 +4,10 @@ class Data extends React.Component {
     constructor() {
         super();
         this.state = {
+            input: null,
             forms: null
         }
-        this.showStalkingCount = this.showStalkingCount.bind(this);
+        this.showVictimizationCount = this.showVictimizationCount.bind(this);
     }
 
     async componentDidMount() {
@@ -15,35 +16,83 @@ class Data extends React.Component {
         this.setState({forms: formsObj})
     }
 
-    showStalkingCount (forms) {
+    showVictimizationCount (forms, incident) {
         if (forms) {
             let theCount = 0;
             forms.forEach((form) => {
                 let incidentArray = form.data.incidents;
-                incidentArray.forEach((incident) => {
-                    if (incident.victimization.includes("Stalking")) {
+                incidentArray.forEach((incidentTwo) => {
+                    if (incidentTwo.victimization.includes(incident)) {
                         theCount++;
+                        console.log(incident)
                     }
                 })
             })
-            return <p>this many stalking incidents occurred: {theCount}</p>
+            return <p>This many {incident}'s occurred: {theCount}</p>
         } else {
             return <p>loading form data...</p>;
         }
+    }
+
+    showHomelessCount(forms) {
+        if (forms && document.getElementById("Homelessness").value === "Homeless") {
+            let theCount = 0;
+            forms.forEach((form) => {
+                console.log(form.data.miscChars)//this is an array.
+                let homelessArray = form.data.miscChars;
+                    //console.log(homeless)//this is actually a string
+                    if (homelessArray.includes("Homeless")) {
+                        theCount++;
+                    }
+
+             })
+            return <p>This many are homeless: {theCount}</p>
+        }else{
+            return <p>fetching homeless data...</p>
+        }
+    }
+
+    handleInputChange = () => {
+   this.setState({input: document.getElementById("input").value})
     }
 
     render() {
       return (
           <div id="data-page">
             <h1>this is the data page</h1>
-            <form>
-            <input
-            placeholder="Search for..."
-            ref={input => this.search = input}
-            onChange={this.handleInputChange}
-            />
+            <form id="Victimizations">
+            <select id="input" onChange={this.handleInputChange}>
+            <option  disabled selected value="">Victimization</option> 
+            <option value="Rape">Rape</option>
+            <option value="Attempted Rape">Attempted Rape</option>
+            <option value="Sex Trafficking">Sex Trafficking</option>
+            <option value="Child Sexual Abuse">Child Sexual Abuse</option>
+            <option value="Drug Facilitated SV">Drug Facilitated SV</option>
+            <option value="Stalking">Stalking</option>
+            <option value="Sexaul Harassment">Sexaul Harassment</option>
+            <option value="Domestic Violence">Domestic Violence</option>
+            <option value="Other">Other</option>
+            </select>
             </form>
-            {this.showStalkingCount(this.state.forms)}
+            <form>
+            <select id="Homelessness" onChange={this.handleInputChange}>
+            <option disabled selected value="">Homelessness</option> 
+            <option value="Homeless">Yes</option> 
+            <option value="">No</option>           
+            </select>
+            </form>
+            <form>
+            <select id="Schools" onChange={this.handleInputChange}>
+            <option disabled selected value="">Schools</option> 
+            <option value="UVM">University of Vermont</option>
+            <option value="Champlain">Champlain College</option>
+            <option value="CCV">Community College Of Vermont</option>
+            <option value="St.Mikes">Saint Michaels College</option>
+            </select>
+            </form>
+            
+            {this.showVictimizationCount(this.state.forms, this.state.input)}
+            {this.showHomelessCount(this.state.forms)}
           </div>
            
       )
