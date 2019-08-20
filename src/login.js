bf356ad8c3027db2f22fc9fb2df801ff29962868
Django Koenig <django.koenig@gmail.com>
2019-08-20 19:09:18 -0400

    css and logo to login page

--- src/login.js
+++ src/login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 // import { Link } from "react-router-dom";
+import Hw from '/Users/spotspot/code/hopeworks/src/images/hw.png';
 
 class Login extends React.Component {
     constructor() {
@@ -11,18 +12,19 @@ class Login extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>HOPEWORKS </h1>
-                <div class="main">
-                    <div class="w3">
-                        <div class="signin-form profile">
-                            <h3>Login</h3>
-                            <div class="login-form">
+          <div className="login-container">
+             <div className="login-contents">  
+               <img className="hw-logo" src={Hw} alt="Hope Works"/>
+                <div className="main">
+                    <div className="w3">
+                        <div className="signin-form profile">
+                            <h3 id="login-text">We Believe Survivors. We Support Survivors.</h3>
+                            <div className="login-form">
                                 <form action="/" method="post">
-                                    <input type="text" name="logemail" placeholder="E-mail" required="" />
-                                    <input type="password" name="logpassword" placeholder="Password" required="" />
-                                    <div class="tp">
-                                        <input type="submit" value="LOGIN NOW" />
+                                    <input id="login-box" type="text" name="logemail" placeholder="E-mail" required="" />
+                                    <input id="login-box" type="password" name="logpassword" placeholder="Password" required="" />
+                                    <div className="tp">
+                                        <input className="login-button" type="submit" value="LOGIN NOW" />
                                     </div>
                                 </form>
                             </div>
@@ -30,10 +32,9 @@ class Login extends React.Component {
                     </div>
                 </div>
             </div>
+         </div> 
         )
     }
 }
 
 export default Login;
-
-
