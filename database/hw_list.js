9007347f2d6d866679321c2af029c1bc7173ea5f
aaronlav <51305085+aaronlav@users.noreply.github.com>
2019-08-20 16:30:16 -0400

    hw changes -8/20

--- database/hw_list.js
+++ database/hw_list.js
@@ -1,13 +1,13 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
-const url = 'mongodb://localhost:27017';
-const moment = require('moment');
-const client = new MongoClient(url);
-const ObjectId = require('mongodb').ObjectId;
+//const url = 'mongodb://localhost:27017';
+//const moment = require('moment');
+//const client = new MongoClient(url);
 
-module.exports = class DataStore {
+
+class FactStore {
   constructor(dbUrl) {
-    this.dbUrl = dbUrl;
+    this.dbUrl = dbUrl;                      //FactStore class which references "hw" database from (currently) local drive
     this.dbClient = null;
     this.dbName = 'hw';
   }
@@ -16,19 +16,27 @@ module.exports = class DataStore {
     if (this.dbClient && this.dbClient.isConnected()) {
       return this.dbClient;
     } else {
-      // http://mongodb.github.io/node-mongodb-native/3.1/api/MongoClient.html
+      // http://mongodb.github.io/node-mongodb-native/3.1/api/MongoClient.html             //async function connecting to database
       console.log(`Connecting to ${this.dbUrl}...`)
       this.dbClient = await MongoClient.connect(this.dbUrl, { useNewUrlParser: true })
-      console.log("Connected to database.");
+      console.log("Connected to database: " + this.dbName);
       return this.dbClient;
     }
   }
 
+
+
   async collection() {
     const client = await this.client();
     const db = client.db(this.dbName);
-    const collection = db.collection('hw');
+    const collection = db.collection('users');                    //references 'users' collection within local 'hw' database
     return collection;
+  }
+
+  async getUser(user) {
+    const u = user
+    const collection = await this.collection();                //'getUser' function which is currently being used by 'server.js'
+    return collection.find({user: u}).sort([['when', 1]]);
   }
 
   async all() {
@@ -56,25 +64,32 @@ module.exports = class DataStore {
   printEntry(fact, currentDay) {
   }
 
-  async addForm(data) {
+  async addForm(text) {
     let entry = {
-      when: new Date(),
-      data: data
+      when: new Date(),                        //adding a form ('addForm') in text
+      text: text
     };
 
     let collection = await this.collection()
-    let result = await collection.insertOne(entry)
+    let result = await collection.insertOne(entry)                           //"sanity check" which is unused at the moment
     assert.equal(1, result.insertedCount); // sanity check
     console.log('Inserted fact as id ' + result.insertedId)
 
     return {id: result.insertedId};
   }
 
-  async deleteForm(id) {
-    let collection = await this.collection()
-    const objectId = new ObjectId(id)
-    let query = {_id: objectId}
-    collection.remove(query)
+  async deleteFact(text) {
+    let entry = {                     //"deleteFact" will in theory be renamed to something like "deleteUser"
+      text: text
+    };
+
+     let collection = await this.collection()
+     let result = await collection.deleteOne(entry)
+     //assert.equal(1, result.insertedCount); // sanity check
+     console.log('Deleted fact as id ' + result.deltetedCount)
+
+     return {numDeleted: result.deltetedCount};
+  
  }
 }
 
@@ -132,3 +147,5 @@ module.exports = class DataStore {
 
 //front end sends post requests to express.... express calls off to middleware, then calls to db
 // })
+
+module.exports = FactStore
