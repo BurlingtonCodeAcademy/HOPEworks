9c55824a92e61da6f9f3c6ad6c0f83cae57a1ff0
aaronlav <51305085+aaronlav@users.noreply.github.com>
2019-08-20 16:50:31 -0400

    redo! -a

--- database/hw_list.js
+++ database/hw_list.js
@@ -1,9 +1,8 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
-//const url = 'mongodb://localhost:27017';
-//const moment = require('moment');
-//const client = new MongoClient(url);
-
+const url = 'mongodb://localhost:27017';
+const moment = require('moment');
+const ObjectId = require('mongodb').ObjectId;
 
 class FactStore {
   constructor(dbUrl) {
@@ -23,8 +22,6 @@ class FactStore {
       return this.dbClient;
     }
   }
-
-
 
   async collection() {
     const client = await this.client();
@@ -46,20 +43,34 @@ class FactStore {
 
   async printAll() {
     let cursor = await this.all();
+  
+module.exports = class DataStore {
+  constructor(dbUrl) {
+module.exports = class DataStore {
+    return {id: result.insertedId};
+  }
 
-  //   let currentDay;
-  //   await cursor.forEach((fact) => {
-  //     let when = moment(fact.when);
-  //     let startOfDay = when.format('YYYYMMDD');
-  //     if (!currentDay || currentDay != startOfDay) {
-  //       console.log(when.format('MMMM Do, YYYY'));
-  //       currentDay = startOfDay;
-  //     }
-  //     let output = when.format('  hh:mm a - ') + fact.text;
-  //     console.log(output);
-  //     return currentDay;
-  //   })
-   }
+  async deleteFact(data) {
+    let entry = {
+      data: data
+    };
+
+     let collection = await this.collection()
+     let result = await collection.deleteOne(entry)
+     //assert.equal(1, result.insertedCount); // sanity check
+     console.log('Deleted fact as id ' + result.deltetedCount)
+
+     return {numDeleted: result.deltetedCount};
+
+  async deleteForm(id) {
+    let collection = await this.collection()
+    const objectId = new ObjectId(id)
+    let query = {_id: objectId}
+    collection.remove(query)
+ }
+}
+
+  }
 
   printEntry(fact, currentDay) {
   }
@@ -92,7 +103,8 @@ class FactStore {
   
  }
 }
-
+  
+ module.exports = FactStore
 
 // client.connect(error => {
 //     let doc = {firstname: error.firstname} //example
@@ -107,31 +119,21 @@ class FactStore {
 //     client.close();
 // }); js in relation to html
 // MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
-
 //     if (err) throw err;
-
 //     const db = client.db("hw");
-
 // db.createCollection('hw')
-
 //     db.listCollections().toArray().then((docs) => {
-
 //         console.log('Available collections:');
 //         docs.forEach((doc, idx, array) => { console.log(doc.name) });
-
 //     }).catch((err) => {
-
 //         console.log(err);
 //     }).finally(() => {
 //     });
-
-
     //     const col = db.collection('find');
     //     // Insert a single document
     //     col.insertMany([{a:admin}, {a:admin}, {a:admin}], function(err, r) {
     //       assert.equal(null, err);
     //       assert.equal(3, r.insertedCount);
-
     //       // Get first two documents that match the query
     //       col.find({a:1}).toArray(function(err, docs) {
     //         assert.equal(null, err);
@@ -140,12 +142,7 @@ class FactStore {
     //       });
     //     });
     //   });
-
     //     client.close();
     // });
-
-
 //front end sends post requests to express.... express calls off to middleware, then calls to db
 // })
-
-module.exports = FactStore
