54709eb8366c5d33c739b96157546ff12a1f1205
Gabe N <nelsong1997@gmail.com>
2019-08-20 17:58:38 -0400

    this does work

--- database/hw_list.js
+++ database/hw_list.js
@@ -2,11 +2,12 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 const url = 'mongodb://localhost:27017';
 const moment = require('moment');
+const client = new MongoClient(url);
 const ObjectId = require('mongodb').ObjectId;
 
 module.exports = class DataStore {
   constructor(dbUrl) {
-    this.dbUrl = dbUrl;                      //FactStore class which references "hw" database from (currently) local drive
+    this.dbUrl = dbUrl;
     this.dbClient = null;
     this.dbName = 'hw';
   }
@@ -15,9 +16,10 @@ module.exports = class DataStore {
     if (this.dbClient && this.dbClient.isConnected()) {
       return this.dbClient;
     } else {
+      // http://mongodb.github.io/node-mongodb-native/3.1/api/MongoClient.html
       console.log(`Connecting to ${this.dbUrl}...`)
       this.dbClient = await MongoClient.connect(this.dbUrl, { useNewUrlParser: true })
-      console.log("Connected to database: " + this.dbName);
+      console.log("Connected to database.");
       return this.dbClient;
     }
   }
@@ -25,14 +27,8 @@ module.exports = class DataStore {
   async collection() {
     const client = await this.client();
     const db = client.db(this.dbName);
-    const collection = db.collection('users');                    //references 'users' collection within local 'hw' database
+    const collection = db.collection('hw');
     return collection;
-  }
-
-  async getUser(user) {
-    const u = user
-    const collection = await this.collection();                //'getUser' function which is currently being used by 'server.js'
-    return collection.find({user: u}).sort([['when', 1]]);
   }
 
   async all() {
@@ -42,19 +38,32 @@ module.exports = class DataStore {
 
   async printAll() {
     let cursor = await this.all();
-  }
+
+  //   let currentDay;
+  //   await cursor.forEach((fact) => {
+  //     let when = moment(fact.when);
+  //     let startOfDay = when.format('YYYYMMDD');
+  //     if (!currentDay || currentDay != startOfDay) {
+  //       console.log(when.format('MMMM Do, YYYY'));
+  //       currentDay = startOfDay;
+  //     }
+  //     let output = when.format('  hh:mm a - ') + fact.text;
+  //     console.log(output);
+  //     return currentDay;
+  //   })
+   }
 
   printEntry(fact, currentDay) {
   }
 
   async addForm(data) {
     let entry = {
-      when: new Date(),                        //adding a form ('addForm') in text
+      when: new Date(),
       data: data
     };
 
     let collection = await this.collection()
-    let result = await collection.insertOne(entry)                           //"sanity check" which is unused at the moment
+    let result = await collection.insertOne(entry)
     assert.equal(1, result.insertedCount); // sanity check
     console.log('Inserted fact as id ' + result.insertedId)
 
@@ -67,17 +76,59 @@ module.exports = class DataStore {
     let query = {_id: objectId}
     collection.remove(query)
  }
-
-  async deleteFact(text) {
-    let entry = {                     //"deleteFact" will in theory be renamed to something like "deleteUser"
-      text: text
-    };
-
-     let collection = await this.collection()
-     let result = await collection.deleteOne(entry)
-     //assert.equal(1, result.insertedCount); // sanity check
-     console.log('Deleted fact as id ' + result.deltetedCount)
-
-     return {numDeleted: result.deltetedCount};
- }
 }
+
+
+// client.connect(error => {
+//     let doc = {firstname: error.firstname} //example
+//     assert.equal(null, error)
+//     console.log('Working!')
+//     const db = client.db(dbName)
+//     db.collection('users').insertOne({user:1}, (error, result) => {
+//         assert.equal(null, error)
+//         assert.equal(1, result.insertedCount)
+//         console.log('insertedCount' + result.insertedCount)
+//     });
+//     client.close();
+// }); js in relation to html
+// MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+
+//     if (err) throw err;
+
+//     const db = client.db("hw");
+
+// db.createCollection('hw')
+
+//     db.listCollections().toArray().then((docs) => {
+
+//         console.log('Available collections:');
+//         docs.forEach((doc, idx, array) => { console.log(doc.name) });
+
+//     }).catch((err) => {
+
+//         console.log(err);
+//     }).finally(() => {
+//     });
+
+
+    //     const col = db.collection('find');
+    //     // Insert a single document
+    //     col.insertMany([{a:admin}, {a:admin}, {a:admin}], function(err, r) {
+    //       assert.equal(null, err);
+    //       assert.equal(3, r.insertedCount);
+
+    //       // Get first two documents that match the query
+    //       col.find({a:1}).toArray(function(err, docs) {
+    //         assert.equal(null, err);
+    //         assert.equal(2, docs.length);
+    //         client.close();
+    //       });
+    //     });
+    //   });
+
+    //     client.close();
+    // });
+
+
+//front end sends post requests to express.... express calls off to middleware, then calls to db
+// })
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 const app = express();
 const DataStore = require('./database/hw_list')
 const assert = require('assert');
+//const bcrypt = require('bcrypt');
+//const dbUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 const store = new DataStore('mongodb+srv://HOPEworksAdmin:HOPEworks1337@hopeworks-data-asjmw.mongodb.net/test?retryWrites=true&w=majority');
 
 MongoClient.connect('mongodb+srv://HOPEworksAdmin:HOPEworks1337@hopeworks-data-asjmw.mongodb.net/test?retryWrites=true&w=majority', (err, client) => {
@@ -11,14 +13,18 @@ MongoClient.connect('mongodb+srv://HOPEworksAdmin:HOPEworks1337@hopeworks-data-a
   let db = client.db('hw') // hw --> users
   console.log('connected to mongo')
 })
+
 app.get('/forms', getAll);
+
 app.get('/delete/:id', deleteForm)
+
 async function deleteForm (request, response) {
   let id = request.params.id
   let result = await store.deleteForm(id)
   console.log(result)
   response.redirect("/forms")
 }
+
 async function getAll(request, response) {
   let cursor = await store.all();
   let output = [];
@@ -32,10 +38,6 @@ async function getAll(request, response) {
   });
 }
 
-app.get('/forms', getAll);
-
-app.get('/delete/:id', deleteForm)
-
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.set('view engine', 'ejs')
@@ -45,9 +47,6 @@ app.listen(5000, function () {
 })
 
 app.post('/form', express.json(), (req, res) => {
-  console.log("express post says, the request is: ")
-  console.log(req.body) 
-  console.log(res.body)
   addData(req, res)
 })
 
@@ -61,6 +60,34 @@ async function addData(request, response) {
     .type('application/json')
     .send(JSON.stringify(output))
 }
+
 app.post('/login', (req, res) => {
   console.log(req.body)
+
+if (req.body.email &&
+  req.body.username &&
+  req.body.password &&
+  req.body.passwordConf) {
+
+  var userData = {
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password,
+  }
+} else if (req.body.logemail && req.body.logpassword) {
+  User.authenticate(req.body.logemail, req.body.logpassword, function (error, user) {
+    if (error || !user) {
+      var err = new Error('Wrong email or password.');
+      err.status = 401;
+      return res.redirect('/');
+    }
+//    bcrypt.compare(password, user.password, function (err, result) {
+//      if (result === true) {
+//        return callback(null, user);
+//      } else {
+//        return callback();
+//      }    
+//    })
+  });
+}
 })
