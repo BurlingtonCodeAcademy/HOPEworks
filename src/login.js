a3f9ef4fd429ec6e7d02f24381116ef997abaf6a
Django Koenig <django.koenig@gmail.com>
2019-08-20 20:58:29 -0400

    login page css and google button

--- src/login.js
+++ src/login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import { Link } from "react-router-dom";
 import Hw from '/Users/spotspot/code/hopeworks/src/images/hw.png';
+import SignIn from '/Users/spotspot/code/hopeworks/src/images/sign-in-google.png';
 
 class Login extends React.Component {
     constructor() {
@@ -13,26 +14,28 @@ class Login extends React.Component {
     render() {
         return (
           <div className="login-container">
-             <div className="login-contents">  
-               <img className="hw-logo" src={Hw} alt="Hope Works"/>
+          <div className="login-contents">  
+            <img className="hw-logo" src={Hw} alt="Hope Works"/>
+              <div className="w3">
+               <div className="signin-form profile">
+                <h3 id="login-message">We Believe Survivors. We Support Survivors.</h3>
                 <div className="main">
-                    <div className="w3">
-                        <div className="signin-form profile">
-                            <h3 id="login-text">We Believe Survivors. We Support Survivors.</h3>
-                            <div className="login-form">
-                                <form action="/" method="post">
-                                    <input id="login-box" type="text" name="logemail" placeholder="E-mail" required="" />
-                                    <input id="login-box" type="password" name="logpassword" placeholder="Password" required="" />
-                                    <div className="tp">
-                                        <input className="login-button" type="submit" value="LOGIN NOW" />
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-         </div> 
+                 <div className="login-form">
+                  <form action="/" method="post">
+                    <input id="login-box" type="text" name="logemail" placeholder="E-mail" required="" />
+                    <input id="login-box" type="password" name="logpassword" placeholder="Password" required="" />
+                     <div className="tp">
+                     <input className="login-button" type="submit" value="LOGIN" />
+                     </div>
+                     <br />
+                     <img className="google-logo" src={SignIn} alt="Sign in with Google"/>
+                    </form>
+                   </div>
+                  </div>
+              </div>
+             </div>
+           </div>
+          </div> 
         )
     }
 }
